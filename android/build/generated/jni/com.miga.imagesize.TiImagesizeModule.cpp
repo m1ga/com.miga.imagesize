fb48448b9f227de302a7113578a729f6ca49bfd8
Michael <migamiga@web.de>
2016-06-18 13:04:11 +0200

    init

---
+++ android/build/generated/jni/com.miga.imagesize.TiImagesizeModule.cpp
@@ -0,0 +1,194 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "com.miga.imagesize.TiImagesizeModule.h"
+
+#include "AndroidUtil.h"
+#include "EventEmitter.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "Proxy.h"
+#include "ProxyFactory.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+
+#include "com.miga.imagesize.ExampleProxy.h"
+
+#include "org.appcelerator.kroll.KrollModule.h"
+
+#define TAG "TiImagesizeModule"
+
+using namespace v8;
+
+		namespace com {
+		namespace miga {
+		namespace imagesize {
+
+
+Persistent<FunctionTemplate> TiImagesizeModule::proxyTemplate = Persistent<FunctionTemplate>();
+jclass TiImagesizeModule::javaClass = NULL;
+
+TiImagesizeModule::TiImagesizeModule(jobject javaObject) : titanium::Proxy(javaObject)
+{
+}
+
+void TiImagesizeModule::bindProxy(Handle<Object> exports)
+{
+	if (proxyTemplate.IsEmpty()) {
+		getProxyTemplate();
+	}
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("TiImagesize");
+
+	Local<Function> proxyConstructor = proxyTemplate->GetFunction();
+	Local<Object> moduleInstance = proxyConstructor->NewInstance();
+	exports->Set(nameSymbol, moduleInstance);
+}
+
+void TiImagesizeModule::dispose()
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Dispose();
+		proxyTemplate = Persistent<FunctionTemplate>();
+	}
+
+	titanium::KrollModule::dispose();
+}
+
+Handle<FunctionTemplate> TiImagesizeModule::getProxyTemplate()
+{
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate;
+	}
+
+	LOGD(TAG, "GetProxyTemplate");
+
+	javaClass = titanium::JNIUtil::findClass("com/miga/imagesize/TiImagesizeModule");
+	HandleScope scope;
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("TiImagesize");
+
+	Handle<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(
+		titanium::KrollModule::getProxyTemplate()
+, javaClass, nameSymbol);
+
+	proxyTemplate = Persistent<FunctionTemplate>::New(t);
+	proxyTemplate->Set(titanium::Proxy::inheritSymbol,
+		FunctionTemplate::New(titanium::Proxy::inherit<TiImagesizeModule>)->GetFunction());
+
+	titanium::ProxyFactory::registerProxyPair(javaClass, *proxyTemplate);
+
+	// Method bindings --------------------------------------------------------
+	DEFINE_PROTOTYPE_METHOD(proxyTemplate, "getSize", TiImagesizeModule::getSize);
+
+	Local<ObjectTemplate> prototypeTemplate = proxyTemplate->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = proxyTemplate->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return proxyTemplate;
+}
+
+// Methods --------------------------------------------------------------------
+Handle<Value> TiImagesizeModule::getSize(const Arguments& args)
+{
+	LOGD(TAG, "getSize()");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		return titanium::JSException::GetJNIEnvironmentError();
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(TiImagesizeModule::javaClass, "getSize", "(Lorg/appcelerator/kroll/KrollDict;)Lorg/appcelerator/kroll/KrollDict;");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'getSize' with signature '(Lorg/appcelerator/kroll/KrollDict;)Lorg/appcelerator/kroll/KrollDict;'";
+			LOGE(TAG, error);
+				return titanium::JSException::Error(error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(args.Holder());
+
+	if (args.Length() < 1) {
+		char errorStringBuffer[100];
+		sprintf(errorStringBuffer, "getSize: Invalid number of arguments. Expected 1 but got %d", args.Length());
+		return ThrowException(Exception::Error(String::New(errorStringBuffer)));
+	}
+
+	jvalue jArguments[1];
+
+
+
+
+	bool isNew_0;
+	
+	if (!args[0]->IsNull()) {
+		Local<Value> arg_0 = args[0];
+		jArguments[0].l =
+			titanium::TypeConverter::jsObjectToJavaKrollDict(env, arg_0, &isNew_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+
+	jobject javaProxy = proxy->getJavaObject();
+	jobject jResult = (jobject)env->CallObjectMethodA(javaProxy, methodID, jArguments);
+
+
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+			if (isNew_0) {
+				env->DeleteLocalRef(jArguments[0].l);
+			}
+
+
+	if (env->ExceptionCheck()) {
+		Handle<Value> jsException = titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+		return jsException;
+	}
+
+	if (jResult == NULL) {
+		return Null();
+	}
+
+	Handle<Value> v8Result = titanium::TypeConverter::javaObjectToJsValue(env, jResult);
+
+	env->DeleteLocalRef(jResult);
+
+
+	return v8Result;
+
+}
+
+// Dynamic property accessors -------------------------------------------------
+
+
+		} // imagesize
+		} // miga
+		} // com
